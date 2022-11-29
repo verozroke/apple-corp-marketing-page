4a283bb88a0d6958f42ef31e2ffeee245caf7e07
Askhat <73778760+verozroke@users.noreply.github.com>
2022-11-29 07:07:29 +0600

    added animations

--- #source/ts/app.ts
+++ #source/ts/app.ts
@@ -1,5 +1,29 @@
 let ellen: string = ' i love you ellen <3'
 console.log(ellen)
 
+let training1: Element = document.getElementsByClassName('courses__el')[0]
+let training2: Element = document.getElementsByClassName('courses__el')[1]
+let training3: Element = document.getElementsByClassName('courses__el')[2]
+let training4: Element = document.getElementsByClassName('courses__el')[3]
+
+
+function makeActive(id: string): void {
+    if (training1.classList.contains('active')) {
+        training1.classList.remove('active')
+    }
+    if (training2.classList.contains('active')) {
+        training2.classList.remove('active')
+    }
+    if (training3.classList.contains('active')) {
+        training3.classList.remove('active')
+    }
+    if (training4.classList.contains('active')) {
+        training4.classList.remove('active')
+    }
+    const givenEl: Element | null = document.getElementById(id)
+    givenEl?.classList.toggle('active')
+}
+
+
 // @ts-ignore
 export { };
--- js/app.js
+++ js/app.js
@@ -20,6 +20,41 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 var ellen = ' i love you ellen <3';
 console.log(ellen);
+var training1 = document.getElementsByClassName('trainings__el')[0];
+var training2 = document.getElementsByClassName('trainings__el')[1];
+var training3 = document.getElementsByClassName('trainings__el')[2];
+var training4 = document.getElementsByClassName('trainings__el')[3];
+
+function makeActive(id) {
+    if (training1.classList.contains('active')) {
+        training1.classList.remove('active');
+    }
+    if (training2.classList.contains('active')) {
+        training2.classList.remove('active');
+    }
+    if (training3.classList.contains('active')) {
+        training3.classList.remove('active');
+    }
+    if (training4.classList.contains('active')) {
+        training4.classList.remove('active');
+    }
+    var givenEl = document.getElementById(id);
+    givenEl === null || givenEl === void 0 ? void 0 : givenEl.classList.toggle('active');
+}
+
+
+training1.addEventListener('click', ()=> {
+    makeActive('el1')
+})
+training2.addEventListener('click', ()=> {
+    makeActive('el2')
+})
+training3.addEventListener('click', ()=> {
+    makeActive('el3')
+})
+training4.addEventListener('click', ()=> {
+    makeActive('el4')
+})
 
 
 /******/ })()
